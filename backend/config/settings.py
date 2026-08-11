0fb0cd4bc60d508850b2e199337afe369144789d
Lev Breslav <leobreslav@gmail.com>
2026-08-11 20:52:54 +0300

    Кастомная модель пользователя с email вместо username

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -35,7 +35,11 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    'accounts',
 ]
+
+AUTH_USER_MODEL = "accounts.User"
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
